6a9c0c242f9ae2208f336e0741f97a52e620f066
Jan Linder <jlind112@gmail.com>
2023-03-30 10:58:02 +0200

    Set up packages

--- bn.R
+++ bn.R
@@ -1,4 +1,11 @@
 # install.packages("bnlearn")
+# install.packages("BiocManager")
+# BiocManager::install("Rgraphviz")
 
 library(bnlearn)
+library(Rgraphviz)
 
+nodes <- c("animal welfare", "environmental impact")
+
+network <- empty.graph(nodes)
+graphviz.plot(network, layout = "dot")
